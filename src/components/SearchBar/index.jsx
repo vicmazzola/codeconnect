80c3cf70394fbfa63cb7bdb03275b74d6c01a263
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-20 19:11:20 -0300

    fix search img

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,7 +1,12 @@
 import './styles.css'
+import searchIcon from './assets/search.svg';
 
 export default function SearchBar () {
     return (
-        <input type='search' placeholder="Type what you are searching" className='search-bar'/>
+        <input type='search' placeholder="Type what you are searching" className='search-bar' style={{
+            background: `var(--dark-grey) url(${searchIcon}) no-repeat 1em center`,
+        }}/>
     )
 }
+
+
