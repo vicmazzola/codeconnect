c9e7df83650351469b8fb3ecb6906bb12a3d4999
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-20 17:46:58 -0300

    start style aside

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -3,13 +3,14 @@ import Feed from './assets/feed.svg';
 import Account from './assets/account_circle.svg';
 import Info from './assets/info.svg';
 import Logout from './assets/logout.svg';
+import './styles.css';
 
 export default function Sidebar() {
     return(
             <aside>
                 <img src={Logo} alt='Codeconnect Logo' />
                 <nav>
-                    <ul>
+                    <ul className='sidebar-list'>
                         <li>
                             <a href='#'>Post</a>
                         </li>
