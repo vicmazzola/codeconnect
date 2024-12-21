81dbe1b408b421d8ed214bf14e694b9ea6c52907
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-21 10:53:39 -0300

    make index.jsx card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,40 @@
+import Banner from './assets/card_code_editor.png';
+import Code from './code.svg';
+import Chat from './chat.svg';
+import Share from './share.svg';
+
+export default function Card() {
+    return (
+        <article className='card'>
+            <div className='card__img'>
+                <img src={Banner} alt='Post image' />
+            </div>
+            <div className='card__content'>
+                <div className='content__text'>
+                    <h3>Post title</h3>
+                    <p>Post overview</p>
+                </div>
+
+                <div className='content__footer'>
+                    <ul>
+                        <li>
+                            <img src={Code} alt='Code'/>
+                            100
+                        </li>
+                        <li>
+                            <img src={Share} alt='Share' />
+                            12
+                        </li><li>
+                            <img src={Chat} alt='Chat' />
+                            10
+                        </li>
+                    </ul>
+
+                    <div className='user__footer'>
+                        
+                    </div>
+                </div>
+            </div>
+        </article>
+    )
+}
