e7391afbc44d21dea6b1231e8edbf621e14775cb
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-21 07:22:59 -0300

    add filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import Filter from "./components/Filter";
 import SearchBar from "./components/SearchBar";
 import Sidebar from "./components/sidebar";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Sidebar />
-      <SearchBar />
+      <div>
+        <SearchBar />
+        <Filter />
+      </div>
     </div>
   );
 }
---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,10 @@
+export default function Filter() {
+    return (
+        <section className='filter-container'>
+            <ul>
+                <li>Programming</li>
+            </ul>
+            <button>Remove All</button>
+        </section>
+    )
+}
